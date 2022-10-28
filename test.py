5e9c92aec66d1537d3592821c791413bdbf9e823
porter-wang <retr0p@outlook.com>
2022-10-29 03:07:33 +0800

    V0.1, very basic rn

--- test.py
+++ test.py
@@ -5,7 +5,7 @@
 #   Format those info
 #   Add those info to respective CSV fields
 #   Download audio file respectively and put them in a directory
-
+import pandas
 import pandas as pd
 import requests as rq
 import json
@@ -23,9 +23,9 @@ def getWordList(file_path):
     path = file_path
     alpha = lambda x: str(x)
     word_csv = pd.read_csv(path, index_col=0, converters={'Word': alpha})
-    word_series = word_csv.loc[:TESTNUM, "Word"]
-    print(f"Debug: the current word_series is: \b {word_series}")
-    print(f"Debug: the word_series contains {word_series.dtype} type data.")
+    word_series = word_csv.loc[:, "Word"]
+    # print(f"Debug: the current word_series is: \b {word_series}")
+    # print(f"Debug: the word_series contains {word_series.dtypes} type data.")
     return word_series
 
 
@@ -42,12 +42,12 @@ def getJSON(given_word):
 # NOTE: function to parse json into respective fields
 #   Parameter:  json file
 #   Return:     a container that contains word, phonetics, part of speech, audio file path, definition 1 to 3 (if exists), examples 1 to 3,
-def parseInfo(returned_json):
+def parseInfo(returned_json, word):
     info = returned_json
     # print(info)
-    print(f"Returned JSON looks like this: {info}")
+    # print(f"Returned JSON looks like this: {info}")
     word_dict = {
-        "word_base": "",
+        "word_base": str(word),
         "word_phonetic": "",
         "word_audio_url": "",
 
@@ -89,33 +89,36 @@ def parseInfo(returned_json):
     prefix = "word_"
     for word_group in info:
         # Below gets all the pos populated
-        for pos in word_group["meanings"]:
-            try:
-                # Part of speech
-                word_dict[prefix + "pos" + str(pos_group_counter)] = pos["partOfSpeech"]
+        try:
+            for pos in word_group["meanings"]:
+                try:
+                    # Part of speech
+                    word_dict[prefix + "pos" + str(pos_group_counter)] = pos["partOfSpeech"]
 
-                # Definition
-                single_pos_definition_container = ""
-                for definition in pos["definitions"]:
-                    single_pos_definition_container += definition["definition"]
-                    single_pos_definition_container += ";;;"
-                word_dict[prefix + "def" + str(pos_group_counter)] = single_pos_definition_container
+                    # Definition
+                    single_pos_definition_container = ""
+                    for definition in pos["definitions"]:
+                        single_pos_definition_container += definition["definition"]
+                        single_pos_definition_container += ";;;"
+                    word_dict[prefix + "def" + str(pos_group_counter)] = single_pos_definition_container
 
-                # Synonym
-                single_pos_synonyms_container = ""
-                for syno in pos["synonyms"]:
-                    single_pos_synonyms_container += syno + ","
-                word_dict[prefix + "syn" + str(pos_group_counter)] = single_pos_synonyms_container
+                    # Synonym
+                    single_pos_synonyms_container = ""
+                    for syno in pos["synonyms"]:
+                        single_pos_synonyms_container += syno + ","
+                    word_dict[prefix + "syn" + str(pos_group_counter)] = single_pos_synonyms_container
 
-                # Antonym
-                single_pos_antonyms_container = ""
-                for ant in pos["synonyms"]:
-                    single_pos_antonyms_container += ant + ","
-                word_dict[prefix + "ant" + str(pos_group_counter)] = single_pos_antonyms_container
+                    # Antonym
+                    single_pos_antonyms_container = ""
+                    for ant in pos["synonyms"]:
+                        single_pos_antonyms_container += ant + ","
+                    word_dict[prefix + "ant" + str(pos_group_counter)] = single_pos_antonyms_container
 
-                pos_group_counter = pos_group_counter + 1
-            except:
-                pass
+                    pos_group_counter = pos_group_counter + 1
+                except:
+                    pass
+        except:
+            pass
 
         # Below gets all definitions combined and populated
 
@@ -133,11 +136,17 @@ def audioDownload(audio_file_url):
 # NOTE: Main, initiates other functions and get result.
 def main():
     word_series = getWordList(FP)  # Get the series of word
+    newDictionary = []
     for wd in word_series:
         print("\b")
         print(f"Now processing word: {wd}")
-        current_word_dict = parseInfo(getJSON(wd))
-        print(f"Currently the dictionary entry for {wd} looks like this: {current_word_dict}")
+        current_word_dict = parseInfo(getJSON(wd), wd)
+        # print(f"Currently the dictionary entry for {wd} looks like this: {current_word_dict}")
+        newDictionary.append(current_word_dict)
+    print(f"\b The number of things in newDictionary: {len(newDictionary)}")
+    output = pd.DataFrame.from_records(newDictionary)
+    # print(output.shape)
+    output.to_csv('output.csv', sep=',', encoding='utf-8', columns=[1:])
 
 
 # get word data
